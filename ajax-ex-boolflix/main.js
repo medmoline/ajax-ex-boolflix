0470a59cb2db6629bc4803aad05e3a94c83ae797
med_moline <antonio.medugno1990@gmail.com>
2019-05-28 10:52:05 +0200

    ottimizzazione

--- ajax-ex-boolflix/main.js
+++ ajax-ex-boolflix/main.js
@@ -4,7 +4,9 @@
 // 1. Titolo; 2. Titolo Originale; 3. Lingua; 4. Voto
 
 $(document).ready(function() {
-
+  var url_base = 'https://api.themoviedb.org/3';
+  var source   = $("#entry-template").html();
+  var template = Handlebars.compile(source);
   search();
 
   function search(){
@@ -18,7 +20,7 @@ $(document).ready(function() {
       //chiamata ajax
       $.ajax({
         //url preso dal sito
-        'url': 'https://api.themoviedb.org/3/search/movie',
+        'url': url_base + '/search/movie',
         //data : api key personale del sito e query = valore input
         'data':{
           'api_key': 'bd8c17f057d750aa7d3b3a89931beb84',
@@ -27,31 +29,24 @@ $(document).ready(function() {
         },
         'success': function(data){
           //ciclo for lungo quanto tutto l'array results
-            for(var i=0; i<data.results.length; i++){
-              //se il valore_input è incluso nella proprietà title dell'oggetto results
-              if(! valore_input.toLowerCase().includes(data.results[i].title)){
-                //creo il template
-                var source   = $("#entry-template").html();
-                var template = Handlebars.compile(source);
-                //context = proprietà da mettere nel template
-                var context = {
-                  'titolo': data.results[i].title,
-                  'titolo_originale':data.results[i].original_title ,
-                  'lingua':data.results[i].original_language,
-                  'voto':data.results[i].vote_average
-                };
-                var html    = template(context);
-                //controllo se il titolo scritto e quindi il titolo non originale è uguale a quello originale moostri solo il secondo 
-                if(data.results[i].title == data.results[i].original_title){
-                  $('.card li:first-child').html('');
-                }
-                //appendo tutto al contenitore delle carte
-                $('.container_card').append(html);
-              }
-            }
+            stampa_film(data)
           }
       })
     })
   }
+  function stampa_film(film){
+    for(var i=0; i<film.results.length; i++){
+      var context = {
+          'titolo': film.results[i].title,
+          'titolo_originale':film.results[i].original_title ,
+          'lingua':film.results[i].original_language,
+          'voto':film.results[i].vote_average
+        };
+        var html = template(context);
+        //appendo tutto al contenitore delle carte
+        $('.container_card').append(html);
+    }
+  }
+
 
 })
