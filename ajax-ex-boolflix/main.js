3153d1eba0dd8cafad48f67e3de34c5ad75143ac
med_moline <antonio.medugno1990@gmail.com>
2019-05-28 11:12:22 +0200

    ottimizzazione 2

--- ajax-ex-boolflix/main.js
+++ ajax-ex-boolflix/main.js
@@ -4,44 +4,49 @@
 // 1. Titolo; 2. Titolo Originale; 3. Lingua; 4. Voto
 
 $(document).ready(function() {
+  //salvo in una variabile l'url di base
   var url_base = 'https://api.themoviedb.org/3';
+  //creo template
   var source   = $("#entry-template").html();
   var template = Handlebars.compile(source);
-  search();
 
-  function search(){
-    //intercetto il click sul bottone
-    $('.bottone').click(function() {
-      //resetto sempre l'html ogni volta che parte la ricerca
-      $('.container_card').html('');
-      //salvo il valore dell'input
-      var valore_input = $('.search').val();
-
-      //chiamata ajax
-      $.ajax({
-        //url preso dal sito
-        'url': url_base + '/search/movie',
-        //data : api key personale del sito e query = valore input
-        'data':{
-          'api_key': 'bd8c17f057d750aa7d3b3a89931beb84',
-          'query': valore_input,
-          'language':'it'
-        },
-        'success': function(data){
-          //ciclo for lungo quanto tutto l'array results
-            stampa_film(data)
-          }
-      })
+  //intercetto il click sul bottone
+  $('.bottone').click(function() {
+    //resetto l'html ogni volta che faccio una ricerca
+    $('.container_card').html('');
+    //salvo il valore scritto dall'utente
+    var valore_input = $('.search').val();
+    //chiamo la funzione per far partire la chiamata api
+    chiamata_api_film(valore_input);
+  })
+  //funzione per chiamata_api dei film
+  function chiamata_api_film(testo){
+    $.ajax({
+      //url preso dal sito
+      'url': url_base + '/search/movie',
+      //data : api key personale del sito e query = valore input + lingua
+      'data':{
+        'api_key': 'bd8c17f057d750aa7d3b3a89931beb84',
+        'query':testo,
+        'language':'it'
+      },
+      'success': function(data){
+        //chiamo la funzinone per stampare i film
+          stampa_film(data)
+      }
     })
   }
   function stampa_film(film){
+    //ciclo for lungo quanto tutto l'array
     for(var i=0; i<film.results.length; i++){
+      //context si rifa all'handlebars e gli metto dentro le proprietà da stampare
       var context = {
           'titolo': film.results[i].title,
           'titolo_originale':film.results[i].original_title ,
           'lingua':film.results[i].original_language,
           'voto':film.results[i].vote_average
         };
+        //creo l'html contenente il template 
         var html = template(context);
         //appendo tutto al contenitore delle carte
         $('.container_card').append(html);
