3fa50636caa4e746ab3426e12186c42883e92234
med_moline <antonio.medugno1990@gmail.com>
2019-05-29 02:19:11 +0200

    ricerca serie tv

--- ajax-ex-boolflix/main.js
+++ ajax-ex-boolflix/main.js
@@ -6,25 +6,34 @@
 $(document).ready(function() {
   //salvo in una variabile l'url di base
   var url_base = 'https://api.themoviedb.org/3';
-  var url_film = '/search/movie';
-  //creo template
-  var source   = $("#entry-template").html();
-  var template = Handlebars.compile(source);
+
 
   //intercetto il click sul bottone
   $('.bottone').click(function() {
     //resetto l'html ogni volta che faccio una ricerca
     $('.container_card').html('');
-    //salvo il valore scritto dall'utente
     var valore_input = $('.search').val();
     //chiamo la funzione per far partire la chiamata api
     chiamata_api_film(valore_input,url_film);
-  })
-  //funzione per chiamata_api 
-  function chiamata_api_film(testo,sua_api){
+    chiamata_api_serie(valore_input,url_serie);
+  });
+
+  $('.search').keypress(function(event) {
+    if(event.which == 13){
+      $('.container_card').html('');
+      var valore_input = $('.search').val();
+      console.log(valore_input);
+      chiamata_api_film(valore_input);
+      chiamata_api_serie(valore_input);
+    }
+  });
+
+//funzione per chiamata_api film
+  function chiamata_api_film(testo){
+
     $.ajax({
       //url preso dal sito
-      'url': url_base + sua_api,
+      'url': url_base + '/search/movie',
       //data : api key personale del sito e query = valore input + lingua
       'data':{
         'api_key': 'bd8c17f057d750aa7d3b3a89931beb84',
@@ -33,26 +42,98 @@ $(document).ready(function() {
       },
       'success': function(data){
         //chiamo la funzinone per stampare i film
-          stampa_film(data)
+          stampa_film(data);
       }
     })
   }
+
+//funzione per stampare film
   function stampa_film(film){
+    //template per i film
+    var source   = $("#template_film").html();
+    var template = Handlebars.compile(source);
+    var film = film.results;
+
     //ciclo for lungo quanto tutto l'array
-    for(var i=0; i<film.results.length; i++){
+    for(var i=0; i<film.length; i++){
       //context si rifa all'handlebars e gli metto dentro le proprietà da stampare
       var context = {
-          'titolo': film.results[i].title,
-          'titolo_originale':film.results[i].original_title ,
-          'lingua':film.results[i].original_language,
-          'voto':film.results[i].vote_average
+          'titolo':film[i].title,
+          'titolo_originale':film[i].original_title,
+          'lingua':film[i].original_language,
+          'voto': film[i].vote_average,
+          // 'stelle': stelle(film[i].vote_average)
         };
         //creo l'html contenente il template
         var html = template(context);
         //appendo tutto al contenitore delle carte
         $('.container_card').append(html);
-    }
+      }
+
+  }
+//funzione per chiamata_api serie
+  function chiamata_api_serie(testo){
+    $.ajax({
+      //url preso dal sito
+      'url': url_base + '/search/tv',
+      //data : api key personale del sito e query = valore input + lingua
+      'data':{
+        'api_key': 'bd8c17f057d750aa7d3b3a89931beb84',
+        'query':testo,
+        'language':'it'
+      },
+      'success': function(data){
+        //chiamo la funzinone per stampare i film
+          stampa_serie(data);
+      }
+    })
+  }
+  //funzione per stampare le serie tv
+  function stampa_serie(serie){
+    var source_serie   = $("#template_serie").html();
+    var template_serie = Handlebars.compile(source_serie);
+    var serie = serie.results;
+    //ciclo for lungo quanto tutto l'array
+    for(var i=0; i<serie.length; i++){
+      //context si rifa all'handlebars e gli metto dentro le proprietà da stampare
+      var context_serie = {
+          'titolo':serie[i].name,
+          'titolo_originale':serie[i].original_name,
+          'lingua':serie[i].original_language,
+          'voto': serie[i].vote_average,
+          // 'stelle': stelle(serie[i].vote_average)
+        };
+        //creo l'html contenente il template
+        var html_serie = template_serie(context_serie);
+        //appendo tutto al contenitore delle carte
+        $('.container_card').append(html_serie);
+      }
+
   }
 
 
+
+
+
+  function stelle (numero) {
+    //prendo il numero
+    var meta_numero = numero/2;
+    var numero_corretto = Math.ceil(meta_numero);
+    // console.log(numero);
+    // console.log(meta_numero);
+    // console.log(numero_corretto);
+    var stelle_piene = '';
+    var stelle_vuote = '';
+
+
+    for(var i=0;i<numero_corretto;i++){
+      stelle_piene+= '<i class="fas fa-star"></i> ';
+    }
+
+    for(var j=0;j<(5-numero_corretto);j++){
+      stelle_vuote += '<i class="far fa-star"></i> ';
+    }
+
+    return stelle_piene + stelle_vuote
+  }
 })
